855cb9303b7f2cef7137800a7f928dc1eec359d0
Dennis <dennisl88@berkeley.edu>
2016-02-07 00:15:36 -0800

    Backgrounds

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,40 @@ $(function() {
     });
 });
 
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+$(function() {
+    if ($(window).width() > 768) {
+        var lastScrollHeight = 0;
+        var lastSwitchHeight = 0;
+        $(window).scroll(function(event) {
+           var curTop = $(this).scrollTop();
+           if (curTop == 0) {
+               $(".navbar").css("padding", "20px 0");
+           }
+           if (lastScrollHeight - curTop > 0) {
+               // Scrolled up.
+               $(".navbar").stop().animate({ "marginTop" : "0px" }, 30);
+               if (curTop != 0) {
+                   $(".navbar").css("padding", "0");
+               } else {
+                   $(".navbar").css("padding", "20px 0");
+               }
+               lastSwitchHeight = curTop;
+           }
+           else if (curTop - lastSwitchHeight > 140) {
+               // Scrolled down significantly.
+               $(".navbar").css("padding", "0");
+               $(".navbar").stop().animate({ "marginTop" : "-50px" }, 100);
+               lastSwitchHeight = curTop;
+           }
+           lastScrollHeight = curTop;
+        });
+    }
+});
